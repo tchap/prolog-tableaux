424acd9dd6a3d5ff32adb54c458fc2779941be4f
Ondrej Kupka <ondra.cap@gmail.com>
2012-05-24 18:04:49 +0200

    Rename rules a -> and, o -> or, n -> not

--- tableaux.pl
+++ tableaux.pl
@@ -8,13 +8,11 @@ satisfiable(Formulae) :-
 closed_tableau([X|Set]) :-
 	var(X),
 	X = continue,
-	closed_tableau(Set),
-	!.
+	closed_tableau(Set).
 
 closed_tableau([X|Set]) :-
 	nonvar(X),
-	closed_tableau_b([X|Set]),
-	!.
+	closed_tableau_b([X|Set]).
 
 %% CLOSE
 closed_tableau_b([stop|_]).
@@ -22,16 +20,16 @@ closed_tableau_b([continue|Set]) :-
 	closed_tableau(Set).
 
 %% NOT
-closed_tableau_b([n(continue)|_]).
-closed_tableau_b([n(X)|Set]) :-
+closed_tableau_b([not(continue)|_]).
+closed_tableau_b([not(X)|Set]) :-
 	X = stop,
 	closed_tableau(Set).
 
 %% AND
-closed_tableau_b([a(Phi,Psi)|Set]) :- 
+closed_tableau_b([and(Phi,Psi)|Set]) :- 
 	closed_tableau([Phi,Psi|Set]).
 
 %% OR
-closed_tableau_b([o(Phi,Psi)|Set]) :-
+closed_tableau_b([or(Phi,Psi)|Set]) :-
 	closed_tableau([Phi|Set]),
 	closed_tableau([Psi|Set]).
