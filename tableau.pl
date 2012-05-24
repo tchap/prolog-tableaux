ed8b541971ef7468655a1219ec309da13ac8a0b5
Ondrej Kupka <ondra.cap@gmail.com>
2012-05-24 16:31:29 +0200

    Polish and optimize rules order

--- tableau.pl
+++ tableau.pl
@@ -2,28 +2,36 @@
 %%% Method of analytic tableaux for propositional logic
 %%%
 
-unsatisfiable(Formulae) :- closed_tableau(Formulae).
+satisfiable(Formulae) :-
+	\+ closed_tableau(Formulae).
 
 closed_tableau([X|Set]) :-
 	var(X),
 	X = continue,
-	closed_tableau(Set).
+	closed_tableau(Set),
+	!.
 
 closed_tableau([X|Set]) :-
 	\+ var(X),
-	closed_tableau_b([X|Set]).
+	closed_tableau_b([X|Set]),
+	!.
 
-closed_tableau_b([n(X)|Set]) :-
-	var(X),
-	X = close,
+%% CLOSE
+closed_tableau_b([stop|_]).
+closed_tableau_b([continue|Set]) :-
 	closed_tableau(Set).
 
-closed_tableau_b([close|_]).
+%% NOT
 closed_tableau_b([n(continue)|_]).
+closed_tableau_b([n(X)|Set]) :-
+	X = stop,
+	closed_tableau(Set).
 
-closed_tableau_b([continue|Set]) :- closed_tableau(Set).
+%% AND
+closed_tableau_b([a(Phi,Psi)|Set]) :- 
+	closed_tableau([Phi,Psi|Set]).
 
-closed_tableau_b([a(Phi,Psi)|Set]) :- closed_tableau([Phi,Psi|Set]).
+%% OR
 closed_tableau_b([o(Phi,Psi)|Set]) :-
 	closed_tableau([Phi|Set]),
 	closed_tableau([Psi|Set]).
